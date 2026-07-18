b298a32dbfe566f1a3494562f11ac6c1fed43744
ashrafabdi <shuebabdirizack@gmail.com>
2026-07-18 13:02:53 +0300

    Add JavaScript introduction examples

---
+++ ch1-introduction/script.js
@@ -0,0 +1,22 @@
+// This is a comment
+
+console.log("Hello JavaScript");
+
+
+// Variables
+
+let name = "Shueb";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+
+// Changing HTML content
+
+function changeText() {
+
+    document.getElementById("message").innerHTML =
+    "JavaScript is working!";
+
+}
